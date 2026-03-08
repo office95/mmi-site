a88e7a4eeed7489d93430c62d38fdf4029c29268
Dein Name <deine@mailadresse>
2026-03-08 21:17:00 +0100

    Home video: set iframe loading=lazy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -207,11 +207,12 @@ export default async function Home() {
           </div>
         </section>
 
-        {/* Video Abschnitt */}
+        {/* Video Abschnitt (lazy) */}
         <section className="relative h-[70vh] w-full overflow-hidden bg-black">
           <iframe
             title="MMI Video"
             src="https://player.vimeo.com/video/1169223499?background=1&autoplay=1&loop=1&muted=1&controls=0"
+            loading="lazy"
             className="absolute left-1/2 top-1/2 min-w-[140%] min-h-[140%] -translate-x-1/2 -translate-y-1/2"
             allow="autoplay; fullscreen; picture-in-picture"
             allowFullScreen
